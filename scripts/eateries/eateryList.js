102baa76ceb62499ea2f8a4d2470f89fe8cbadee
Laurel Morrison <lbentleym1992@gmail.com>
2021-03-17 10:13:11 -0400

    playing with event tags

--- scripts/eateries/eateryList.js
+++ scripts/eateries/eateryList.js
@@ -1,39 +1,56 @@
 import { eateryDetail } from './eateryDetail.js';
 import { getEateries, useEateries } from './EateryProvider.js';
 
+export const displayEateries = (eateryArray) => {
+	render(eateryArray)
+  };
 
-export const displayEateries = () => {
-
-	let eateryCollection = [];
-
-	getEateries().then(() => {
-		eateryCollection = useEateries()
-		console.log(eateryCollection, "Let's eat")
-
-	})
-		.then(() => {
-
-			const target = document.querySelector(".eateryDropdown")
-			let eateryHTML = ""
-			for (const eateryObj of eateryCollection) {
-				eateryHTML += eateryDetail(eateryObj)
-			}
-			return target.innerHTML += eateryHTML;
+  const render = (eateryData) => {
+		const eateryDisplay = document.querySelector("#eateryDropdown");
+		let HTMLArray = eateryData.map(eatery => {
+		  return eateryDetail(eatery);
 		})
-};
+		eateryDisplay.innerHTML = HTMLArray.join("");
+  }
+
+const target = document.querySelector("eateryDropdown");
+
+
+target.addEventListener("click", (event) => {
+	if (event.target.id === "eateryDropdown") {
+		filterEatery(".businessName")
+	} else if (event.target.id === "showAll") {
+		displayEateries(useEateries())
+	}
+})
+
+const filterEatery = (whatFilter) => {
+	const filterArray = useEateries().filter(eatery => {
+		if (eatery.businessName.includes(whatFilter)) {
+			return eatery;
+		}
+	})
+	makeEateryList(filterArray);
+}
 
 
 
+// export const displayEateries = () => {
 
-// export const makeEateryList = (eateryArray) => {
-// 	render(eateryArray)
-//   };
+// 	let eateryCollection = [];
 
-//   const render = (eateryData) => {
-// 		const eateryDisplay = document.querySelector("#eateryDropdown");
-// 		let HTMLArray = eateryData.map(eatery => {
-// 		  return eateryDetail(eatery);
+// 	getEateries().then(() => {
+// 		eateryCollection = useEateries()
+// 		console.log(eateryCollection, "Let's eat")
+
+// 	})
+// 		.then(() => {
+
+// 			const target = document.querySelector(".eateryDropdown")
+// 			let eateryHTML = ""
+// 			for (const eateryObj of eateryCollection) {
+// 				eateryHTML += eateryDetail(eateryObj)
+// 			}
+// 			return target.innerHTML += eateryHTML;
 // 		})
-// 		eateryDisplay.innerHTML = HTMLArray.join("");
-//   }
-
+// };
