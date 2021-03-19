22d76ef4b113baf96351ec18a23f0fd9f8442d12
srcrank <salmacrank@gmail.com>
2021-03-19 14:07:16 -0500

    fixing park header css and adding some comments for code

--- scripts/parks/Parks.js
+++ scripts/parks/Parks.js
@@ -16,18 +16,20 @@ export const parkCard = (nps) => {
         imageAltText = imageObject.altText
     }
     return `
-    <h2>${nps.fullName}</h2>
+    <h2 class="parkHeadline">${nps.fullName}</h2>
     <div><img class="parkImg" src="${imageUrl}" alt="${imageAltText}"></div>
     <p>Location: ${nps.states}</p>
     `
 }
 
+//creating HTML component for placing the park into the itinerary preview
 export const parkItin = (nps) => {
     return `
     <h3>${nps.fullName}</h3>
     `
 }
 
+//creating the HTML component for the park more details in the module box. had to specify multiple arrays to populate the contact information. 
 export const moreParkDetails = (nps) => {
     const phoneNumbArray = nps.contacts.phoneNumbers
     const emailArray = nps.contacts.emailAddresses
